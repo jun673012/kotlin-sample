15fd6166a09ca24aa34a43d30eb4022d0635c163
junhyeok <jun673012@gmail.com>
2023-06-10 20:34:45 +0900

    refactor Board entity

--- src/main/kotlin/com/example/ktl/entity/Board.kt
+++ src/main/kotlin/com/example/ktl/entity/Board.kt
@@ -3,15 +3,18 @@ package com.example.ktl.entity
 import jakarta.persistence.*
 
 @Entity
-class Board {
+class Board(
+    title: String,
+    content: String
+) {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
     @Column(nullable = false)
-    var title: String? = null
+    var title: String = title
 
     @Column(nullable = false)
-    var content: String? = null
+    var content: String = content
 }
